de8d98792ba54374123c6857beda57d2ae0c0f08
Jonnie <jonniebarnsley@gmail.com>
2025-01-29 19:12:21 +0000

    first commit

---
+++ libs/count_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# function to count the number of files in a directory which match a given pattern.
+countFiles() {
+    local directory="$1"
+    local pattern="$2"
+    find "$directory" -maxdepth 1 -type f -name "$pattern" | wc -l
+}
