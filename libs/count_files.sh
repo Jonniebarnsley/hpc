34e1c079a7762b2b9d0e99506655ec556ed53d1f
Jonnie <jonniebarnsley@gmail.com>
2025-01-29 19:26:56 +0000

    rename

--- libs/count_files.sh
+++ libs/count_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # function to count the number of files in a directory which match a given pattern.
-countFiles() {
+count_files() {
     local directory="$1"
     local pattern="$2"
     find "$directory" -maxdepth 1 -type f -name "$pattern" | wc -l
