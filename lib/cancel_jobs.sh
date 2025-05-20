f3c1a634635cc83f00d957b9962f2b507ae44dfe
Jonnie <jonniebarnsley@gmail.com>
2025-05-20 17:20:21 +0100

    now cancels all running jobs and no longer takes range of jobids

--- lib/cancel_jobs.sh
+++ lib/cancel_jobs.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-
-usage() { echo "Usage: $0 <job_id_start> <job_id_end>" 1>&2; exit 1; }
-
-if [ "$#" -ne 2 ]; then
-    usage
-fi
-
-START=$1
-END=$2
-
-for jobid in {$START..$END}; do
-    qdel $jobid
+job_ids=$(squeue --me -h -o "%A")
+for id in $job_ids; do 
+	scancel $id
 done
